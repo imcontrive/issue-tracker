c4f7ba39cbbab721b3c0f8a6aa28475f9e6c6c8c
amit gupta <amitg.ghy@gmail.com>
2019-08-07 22:59:26 +0530

    add image

--- client/components/Login.js
+++ client/components/Login.js
@@ -1,9 +1,43 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-function Login(){
-    return(
-        <h1>Login</h1>
-    )
+class Login extends Component {
+  state = {
+    email: "",
+    password: ""
+  };
+
+  changeHandler = e => {
+    const { name, value } = e.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  loginHandler = e => {
+    e.preventDefault();
+    console.log(this.state);
+  };
+  render() {
+    return (
+      <>
+        <form>
+          <input
+            name="email"
+            value={this.state.email}
+            placeholder="email"
+            onChange={this.changeHandler}
+          />
+          <input
+            name="password"
+            value={this.state.password}
+            type="password"
+            onChange={this.changeHandler}
+          />
+          <button onClick={this.loginHandler}>login</button>
+        </form>
+      </>
+    );
+  }
 }
 
-export default Login
+export default Login;
