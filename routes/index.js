47e06d2ffc8bd1fcc96cad4d91e0069167a10550
shubh <psubham94@gmail.com>
2019-08-24 14:59:00 +0530

    webpack.config.prod.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('*', function(req, res, next) {
-  res.render('index', { title: 'issue trackr' });
+
+  const cssPath = process.env.NODE_ENV == 'production' ? `/dist/bundle/bundle.css` : '/static/bundle.css';
+ const jsPath = process.env.NODE_ENV == 'production' ? `/dist/bundle/bundle.js` : '/static/bundle.js';
+  res.render('index', { title: 'issue trackr', cssPath, jsPath });
 });
 
 module.exports = router;
